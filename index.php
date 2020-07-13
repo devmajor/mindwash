f13482e1765202c141d3ea484107335910c478dc
devmajor <victor.mrcertified@gmail.com>
2020-07-13 16:35:41 +0100

    ft-email-capture

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
+<?php
+
+ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    
+
+$to      = 'victor.mrcertified@gmail.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+
+
+if (mail($to, $subject, $message, $headers)) {
+    echo "mail send ... OK";
+} else {
+    echo "mail send ... ERROR";
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>MindWash</title>
 </head>
 <body>
-	<form action="getEmail.php" method="POST">
+	<form action="" method="POST">
 		<input type="text" name="email">
 		<button name="submit">Submit</button>
 	</form>
