9c28b12cc9be04fa4c143322eb8d478e86ec5324
devmajor <victor.mrcertified@gmail.com>
2020-07-13 16:03:53 +0100

    ft-email-capture

--- getEmail.php
+++ getEmail.php
@@ -7,22 +7,37 @@ if(isset($_POST["submit"])){
 echo $to = $_POST["email"];
 $from = "victorabayomi@gmail.com";
 $subject = "MindWash Coming Soon!";
-$message = "Thank you for subscribing, You will be notified when our app is launched"; 
-       
-         $header = "From:victorabayomi@gmail.com \r\n";
-         // $header .= "Cc:afgh@somedomain.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-         
-         $retval = mail ($to,$subject,$message,$header);
-         
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
+$message = "Thank you for subscribing, You will be notified when our app is launched";
+// $message = "
+// <html>
+// <head>
+// <title>HTML email</title>
+// </head>
+// <body>
+// <p>This email contains HTML Tags!</p>
+// <table>
+// <tr>
+// <th>Firstname</th>
+// <th>Lastname</th>
+// </tr>
+// <tr>
+// <td>John</td>
+// <td>Doe</td>
+// </tr>
+// </table>
+// </body>
+// </html>
+// ";
 
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
+// More headers
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
 
 // $separator = md5(time());
 
