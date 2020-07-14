7267841e3865449d5dc374481b125f485f408ccd
devmajor <victor.mrcertified@gmail.com>
2020-07-14 17:36:00 +0100

    ft-landing-page-v1

--- getEmail.php
+++ getEmail.php
@@ -1,30 +1,60 @@
 <?php
+
 ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    
+error_reporting( E_ALL );
+
+$connect = mysqli_connect("localhost","id14333699_mindwashadmin","MINDwash123!","id14333699_mindwash") ;
+
+
+
 if(isset($_POST["submit"])){
-	// $email = $_POST['email'];
+	
 
+$to = $_POST["email"];
+$from = "info@mindwash.com";
+$subject = "MindWash Coming Soon!";
 
-// echo $to = $_POST["email"];
-// $from = "victorabayomi@gmail.com";
-// $subject = "MindWash Coming Soon!";
-// $bdy = "Thank you for subscribing, You will be notified when our app is launched";
+$message = "
+<html>
+    <body style='background:#4285F4; color:#FFFFFF; padding: 2em; font-size:1.5em'>
+    <center>
+        <h1>MindWash</h1>
+    </center>
+    <center>
+        <h4>Thank you for subscribing, You will be notified when our app is launched</h4> <br>
+        
+    </center>
+    </body>
+    </html>
+    ";
 
-$to      = 'victor.mrcertified@gmail.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
+    // Always set content-type when sending HTML email
+$headers = 'From: MindWash info@mindwash.com ' . "\r\n" .
+    'Reply-To: MindWash info@mindwash.com' . "\r\n";
+    $headers .= 'Content-Type: text/html; charset=UTF-8\r\n';
 
 
 if (mail($to, $subject, $message, $headers)) {
-    echo "mail send ... OK";
+    echo "<html><body style='background:#4285F4; padding:10em; color:#FFFFFF; font-family:DM Sans'><center> <h1>Thank you for your interest! </h1>
+        <a href='index.php' style='color:white; text-decoration:none; font-size:1.3em'><< Go Back </a>
+    </center></body></html>";
+    
+    
+    $sql = "INSERT INTO subscribers (`email`)VALUES('$to')";
+    $run_sql = mysqli_query($connect, $sql);
+    
+    if($run_sql){
+       
+    }else{
+        
+    }
+   
 } else {
-    echo "mail send ... ERROR";
+    echo "<html><body style='background:#4285F4; padding:10em; color:#FFFFFF; font-family:DM Sans'> 
+    <center><h1>Something went wrong, try again! </h1></center>
+    </body></html>";
 }
+
 
 
 }else{
--- index.php
+++ index.php
@@ -1,44 +1,70 @@
-<?php
-
-ini_set( 'display_errors', 1 );
-error_reporting( E_ALL );
-    
-if(isset($_GET['email'])){
-
-$email = $_GET['email'];
-
-echo $to      = $email;
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: victorabayomi@gmail.com' . "\r\n" .
-    'Reply-To: victorabayomi@gmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-
-
-if (mail($to, $subject, $message, $headers)) {
-    echo "mail send ... OK";
-} else {
-    echo "mail send ... ERROR";
-}
-
-
-}else{
-	echo "NO DATA";
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>MindWash</title>
+	    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no user-scalable=no"
+    />
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Quicksand:wght@300;400&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<form action="" method="GET">
-		<input type="text" name="email">
-		<button type="submit">Submit</button>
-	</form>
-	
+<div class="top-content">
+	<header>
+		<div class="logo"><h1>MindWash</h1></div>
+		<div class="nav">
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+				<li><button>GET EARLY ACCESS</button></li>
+			</ul>
+		</div>
+	</header>
+	<main>
+		<section>
+			<div class="hero">
+			    <div class="mw-intro-text">
+			        <h1>Erase bad memories with one click, enjoy a happy life!</h1>
+			        <!--<p>Forgeting a bad memory has never been so easy</p>-->
+			        
+			        <div class="mw-subscription-box">
+			            <p>Join the waiting list by entering your email address below,and be among our first esteemed user immediately MindWash is released.</p>
+			            <form action="getEmail.php" method="POST">
+                    		<input type="text" name="email" placeholder="Enter Your Email Address"> <br>
+                    		<button type="submit" name="submit">GET EARLY ACCESS</button>
+                    	</form>
+			        </div>
+			    </div>
+			    <div class="mw-intro-img">
+			        
+			    </div>
+			</div>
+		</section>
+		<section>
+		    <div class="mw-content-wrap">
+		        <div class="mw-content-1">
+		            
+		        </div>
+		        <div class="mw-content-2">
+		            
+		        </div>
+		    </div>
+		</section>
+	</main>
+</div>	
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.0/gsap.min.js"></script>
+<script type="text/javascript">
+		gsap.from(".mw-intro-text", {duration: 1.5, x: -600, opacity: 0, scale: 0.5});
+		gsap.from(".mw-intro-img", {duration: 1.5, x: 600, opacity: 0, scale: 0.5});
+		// gsap.to("#logo", {duration: 1, x: 100});
+	</script> 
 </body>
 </html>
